d12e1b0fe8b4eea23f3f47e84af23b8616d47eff
Jalen Greene <terrell.greene95@gmail.com>
2021-11-07 11:16:05 -0600

    background image and attribution styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import backgroundMobile from "images/pattern-background-mobile.svg";
 import styles from "styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <main>
+    <>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -19,21 +20,25 @@ const Home: NextPage = () => {
 
         <title>Order Summary Card</title>
       </Head>
-      Order Summary You can now listen to millions of songs, audiobooks, and
-      podcasts on any device anywhere you like! Annual Plan $59.99/year Change
-      Proceed to Payment Cancel Order
-      <div className="attribution">
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
-    </main>
+      <main>
+        <Image src={backgroundMobile} />
+        {/* Order Summary You can now listen to millions of songs, audiobooks, and
+        podcasts on any device anywhere you like! Annual Plan $59.99/year Change
+        Proceed to Payment Cancel Order */}
+
+        <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Jalen Greene</a>.
+        </div>
+      </main>
+    </>
   );
 };
 
