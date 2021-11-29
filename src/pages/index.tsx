122c34335364bc1c0f7ee4dd330780533e738ee7
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-11-29 10:17:05 -0600

    Adding home styles and markup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import backgroundMobile from "images/pattern-background-mobile.svg";
+import backgroundDesktop from "images/pattern-background-desktop.svg";
+import hero from "images/illustration-hero.svg";
+import iconMusic from "images/icon-music.svg";
 import styles from "styles/Home.module.scss";
 
 const Home: NextPage = () => {
@@ -20,11 +23,46 @@ const Home: NextPage = () => {
 
         <title>Order Summary Card</title>
       </Head>
-      <main>
-        <Image src={backgroundMobile} />
-        {/* Order Summary You can now listen to millions of songs, audiobooks, and
-        podcasts on any device anywhere you like! Annual Plan $59.99/year Change
-        Proceed to Payment Cancel Order */}
+      <main className={styles["main"]}>
+        <div className={styles["background__mobile"]}>
+          <Image src={backgroundMobile} />
+        </div>
+
+        <div className={styles["background__desktop"]}>
+          <Image src={backgroundDesktop} />
+        </div>
+
+        <section className={styles["section"]}>
+          <Image src={hero} layout="responsive" />
+
+          <div className={styles["section__content"]}>
+            <h1>Order Summary</h1>
+
+            <p>
+              You can now listen to millions of songs, audiobooks, and podcasts
+              on any device anywhere you like!
+            </p>
+
+            <section className={styles["section__plan"]}>
+              <div className={styles["img-container"]}>
+                <Image src={iconMusic} layout="responsive" />
+              </div>
+
+              <div className={styles["price"]}>
+                <span>Annual Plan</span>
+                <span>$59.99/year</span>
+              </div>
+
+              <a href="#">Change</a>
+            </section>
+
+            <button className={styles["section__payment"]}>
+              Proceed to Payment
+            </button>
+
+            <button className={styles["section__cancel"]}>Cancel Order</button>
+          </div>
+        </section>
 
         <div className="attribution">
           Challenge by{" "}
